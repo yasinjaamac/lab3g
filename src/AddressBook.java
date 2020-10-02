c63ddac23706bb0d220cb1a59854d57d9657bae7
yasin <yasinjaamac@cmail.carleton.ca>
2020-10-02 10:42:39 -0400

    code updated

---
+++ src/AddressBook.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+
+    private ArrayList<BuddyInfo> b;
+    public AddressBook(){
+        b = new ArrayList<>();
+
+    }
+    public void addBuddy(BuddyInfo bud){
+        if(bud != null){
+            b.add(bud);
+        }
+    }
+    public BuddyInfo removeBuddy(int I){
+        if(I >= 0 && I < b.size()){
+            return b.remove(I);
+        }
+        return null;
+    }
+
+
+    public static void main (String[] args) {
+        BuddyInfo j = new BuddyInfo("colonel", " jaamac", 4484848);
+        AddressBook adbook = new AddressBook();
+        adbook.addBuddy(j);
+        adbook.removeBuddy( 0);
+
+    }
+
+}
